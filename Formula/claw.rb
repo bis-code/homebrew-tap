022dd1a6830684d946a91e57ae6911ed8876aa8f
bis-code <baicoianuioansorin@gmail.com>
2026-01-08 01:18:48 +0100

    claw 1.3.8 - auto-index projects

--- Formula/claw.rb
+++ Formula/claw.rb
@@ -1,10 +1,10 @@
 class Claw < Formula
   desc "Claude Automated Workflow - CLI tool for Claude Code configurations"
   homepage "https://github.com/bis-code/claw"
-  url "https://github.com/bis-code/claw/releases/download/v1.3.7/claw-1.3.7.tar.gz"
-  sha256 "65afb8799e07c90c909961341a389ee51615f93970cfc1694eb9248a61c2bc2e"
+  url "https://github.com/bis-code/claw/releases/download/v1.3.8/claw-1.3.8.tar.gz"
+  sha256 "9e186ff9f7c5e06c30ce2a3792d6d9e9c74fea9343c2dff15c9aeb4c9e847161"
   license "MIT"
-  version "1.3.7"
+  version "1.3.8"
 
   def install
     bin.install "bin/claw"
@@ -26,6 +26,8 @@ class Claw < Formula
         claw project      - Multi-repo project management
         claw templates    - Manage GitHub issue templates
         claw --help       - Show all options
+
+      New in 1.3.8: Auto-indexes projects for semantic search on first run!
     EOS
   end
 
